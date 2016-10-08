dcb47357c297d61044504dbc734c45667998687c
Loukik Kulkarni <loukik.kulkarni2000@gmail.com>
2016-10-07 20:17:36 -0400

    feat(module-complete): add validations to forms

--- client/AngularJS/user_registration.controller.js
+++ client/AngularJS/user_registration.controller.js
@@ -6,9 +6,11 @@
     angular.module('movieflix')
         .controller('user_registrationController', user_regController);
 
-    user_regController.$inject = ['userService', 'subscriptionService']
-    function user_regController(userService, subscriptionService) {
+    user_regController.$inject = ['userService', 'subscriptionService','$location']
+    function user_regController(userService, subscriptionService,$location) {
         var dataVm = this;
+        dataVm.mm=[];
+        dataVm.yyyy=[];
 
         dataVm.createUser = createUser;
         init();
@@ -20,14 +22,31 @@
                 }, function (error) {
                     console.log(error);
                 });
+            initComponents();
+        }
+
+
+        function initComponents()
+        {
+            for(i=1;i<=12;i++)
+            {
+                dataVm.mm.push(i);
+            }
+            for(i=0;i<10;i++)
+            {
+                dataVm.yyyy.push(new Date().getFullYear()+i);
+            }
         }
 
         function createUser() {
+
            dataVm.user.userPlan=JSON.parse(dataVm.selectedUserPlan);
+           dataVm.user.userPayment.expiryDate=new Date('01-'+dataVm.cardMM+'-'+dataVm.cardYYYY);
             userService.addNew(dataVm.user)
                 .then(function (data) {
                     dataVm.user = data;
                     console.dir("Saved"+ dataVm.user);
+                    $location.path('/');
                 }, function (error) {
                     console.log(error);
                 });
